a20c457313207a7dca194e19dba4ca86c5c6f73c
kobe <kobe@vps-MacBook-Air.local>
2017-08-06 14:41:51 -0700

    edit comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,23 +24,28 @@ module.exports = function(app) {
     // allFriends.push(friendData);
 		// res.json(allFriends);
 
+    //Default friendMatch object
 		var friendMatch = {
 			name: "",
 			image: "",
 			matchDifference: 1000
-		};
-
+    };
+    
+    //totalDifference is set to zero
     var totalDifference = 0;
   
-
+    //loop through every friend stored in allFriends array
 		for(var i = 0; i < allFriends.length; i++){
       console.log(allFriends[i].name);
       totalDifference = 0;
 
+      //find the totalDifference of user input scores and scores of friends in allFriends array
 			for (var j = 0; j < friendScore.length; j++) {
             totalDifference += Math.abs(parseInt(friendScore[j]) - parseInt(allFriends[i].scores[j]));
             console.log("total difference is " + totalDifference);
-          
+
+            //if the totalDifference is less than the default matchDifference, reset name, photo, and matchDifference to the 
+            //name, photo, and totalDifference of the friend
               if (totalDifference < friendMatch.matchDifference){
                 friendMatch.name = allFriends[i].name;
                 friendMatch.photo = allFriends[i].photo;
@@ -50,11 +55,14 @@ module.exports = function(app) {
                 console.log('Friend name = ' + allFriends[i].name);
                 console.log('Friend image = ' + allFriends[i].photo);
               };
-
+            
+            //Reset matchDifference to default at the end of the call
             friendMatch.matchDifference = 1000;
           }
       }
+      //push friendData into allFriends array
       allFriends.push(friendData);
+      //send the friendMatch result in json format
 		  res.json(friendMatch);
 	});
 };
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app = express();
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
+//==============================================================
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
